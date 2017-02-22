11433ce8a7b23127d86f87d033c891ffd56b140f
mattpow35 <mpow2231@740s-pl-121.local>
2017-02-22 08:23:35 -0700

    created project and added ignore files

---
+++ src/regex/controller/RegexRunner.java
@@ -0,0 +1,13 @@
+package regex.controller;
+
+import regex.controller.RegexController;
+
+public class RegexRunner 
+{
+	public static void main (String [] args) 
+	{
+		RegexController appController = new RegexController();
+		appController.start();
+	}
+
+}
