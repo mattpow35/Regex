7b7f44b601cd7b715005bdaf42989f519dc8fbb3
mattpow35 <mpow2231@740s-pl-121.local>
2017-02-22 09:03:16 -0700

    added framework of methods to the controller

--- src/regex/controller/RegexController.java
+++ src/regex/controller/RegexController.java
@@ -1,10 +1,48 @@
 package regex.controller;
 
+import javax.swing.JOptionPane;
+import regex.view.RegexFrame;
+
 public class RegexController 
 {
+	private RegexFrame baseFrame;
+	
+	public RegexController()
+	{
+		this.baseFrame = new RegexFrame(this);
+	}
+	
 	public void start()
 	{
+		JOptionPane.showMessageDialog(baseFrame, "Please enter valid info into each field.");
+	}
+	
+	public boolean isValidFirstName(String input)
+	{
+		boolean valid = false;
 		
+		return valid;
+	}
+	
+	public boolean isValidLastName(String input)
+	{
+		boolean valid = false;
+		
+		return valid;
+	}
+	
+	public boolean isValidPhoneNumber(String input)
+	{
+		boolean valid = false;
+		
+		return valid;
+	}
+	
+	public boolean isValidEmail(String input)
+	{
+		boolean valid = false;
+		
+		return valid;
 	}
 
 }
