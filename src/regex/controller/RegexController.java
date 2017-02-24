a215ee16c56af26b2461b8f22e8a436f47c4a18d
mattpow35 <mpow2231@740s-pl-121.local>
2017-02-24 09:36:23 -0700

    added tests for first and last name

--- src/regex/controller/RegexController.java
+++ src/regex/controller/RegexController.java
@@ -17,16 +17,36 @@ public class RegexController
 		JOptionPane.showMessageDialog(baseFrame, "Please enter valid info into each field.");
 	}
 	
+	//Must be 2-30 characters
+	//Cannot contain symbols.
 	public String isValidFirstName(String input)
 	{
-		String response = "First Name is invalid";
+		String response = "";
+		
+		if (input.length() >= 2 && input.length() <= 30 && input.matches(".*[A-Za-z]"))
+		{
+			response = "valid name";
+		}
+		else
+		{
+			response = "First Name is invalid";
+		}
 		
 		return response;
 	}
 	
 	public String isValidLastName(String input)
 	{
-		String response = "Last Name is invalid";
+		String response = "";
+		
+		if(input.length() >= 2 && input.length() <= 40 && input.matches(".*[A-Za-z].*[-,.'\\w\\s]"))
+		{
+			response = "valid last name";
+		}
+		else
+		{
+			response = "Last name is invalid";
+		}
 		
 		return response;
 	}
